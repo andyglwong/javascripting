b90072d0b449faad5889472fbf0455758043a2b7
Andy <andyglw87@gmail.com>
2018-09-30 04:53:23 +0000

    Objects, Object Properties, and Functions

---
+++ objects.js
@@ -0,0 +1,14 @@
+// //Objects are lists of values similar to arrays, except values are identified by keys instead of integers.
+// Example:
+//  var foodPreferences = {
+//        pizza: 'yum',
+//        salad: 'gross'
+//      };
+
+var pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni'],
+  crust: 'deep dish',
+  serves: 2
+};
+
+console.log(pizza)
